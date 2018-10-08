717a7c9a4e13c7d0cde3d1baeccc7d04b6cbd5de
nawjanimri <alvareztrabajo@gmail.com>
2018-10-08 08:22:26 +0200

    file_3333.py borrado y file_2_new.py renombrado a file_3.py

--- file_3333.py
+++
@@ -1,9 +0,0 @@
-# Archivo 3
-
-print("Editado desde el remote en GitHub")
-
-print("Mejor línea eliminada")
-
-print("Y esta otra añadida")
-
-print("Ya estamos a viernes, y repido la pregunta, ¿llego con fuerzas?")
