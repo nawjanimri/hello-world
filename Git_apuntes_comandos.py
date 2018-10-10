b8264efe6f6b8607ee62279a1b30b6a9ccad39dc
nawjanimri <alvareztrabajo@gmail.com>
2018-10-10 08:16:36 +0200

    mejoras en la descripcion de algunos comandos

--- Git_apuntes_comandos.py
+++ Git_apuntes_comandos.py
@@ -15,15 +15,22 @@
 	> git diff						# Muestra los cambios de archivos modificados pero NO 
 									# añadidos al staging area.
 	> git diff --cached				# Muestra los cambios de archivos modificados que SI 
-									#están añadidos al staging area.
+									# están añadidos al staging area.
+									# Es decir, muestra los cambios entre los archivos del
+									# staging area y los del commit.
 
-	> ¿Cómo mostrar cambios entre repositorio local y remoto?:
+	> ¿Cómo mostrar diferencias entre repositorio local y remoto?:
+		# Por repositorio local se entiende que son los archivos en el staging área o 
+		# en el commit, no los modificados.
 		# Me traigo el repositorio remoto pero sin "merge" con el local:
 		git fetch origin master
 		# Comparo ambos repositorios:
 		git diff <local-branch> <remote repo>/<remote branch>
 		Ej: git diff master origin/master
 		Ej: git diff master mi_remote_repo/master
+
+		# Otra forma de hacerlo:
+		git fetch origin ; git diff --name-only master origin/master
 
 
 	# Ayuda:
